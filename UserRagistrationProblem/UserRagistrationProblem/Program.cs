074ce8e99d5a23a2cb4be29865e32bc20ce37234
Kajal <kajalpatil158@gmail.com>
2021-03-11 12:06:09 +0530

    [Kajal Patil]Added: User_First_Name

---
+++ UserRagistrationProblem/UserRagistrationProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UserRegistrationProblem;
+
+namespace UserRagistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistrationForm registration = new RegistrationForm();
+            Console.WriteLine(registration.Validate("Kajal"));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ UserRagistrationProblem/UserRagistrationProblem/RagistrationForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class RegistrationForm
+    {
+        string First_Name = "^[A-Za-z]";
+        //string Last_Name = "^[A-Za-z]";
+        public bool Validate(string first_name)
+        {
+            return Regex.IsMatch(first_name, First_Name);
+           
+        }
+    }
+}
